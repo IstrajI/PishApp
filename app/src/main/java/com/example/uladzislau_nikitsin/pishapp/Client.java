379c5ad2ac73323a73fa659981fb6130c67ba43c
IstrajI <Mr.Straj@yandex.ru>
2017-05-04 18:42:35 +0300

    Prototype of socket application

---
+++ app/src/main/java/com/example/uladzislau_nikitsin/pishapp/Client.java
@@ -0,0 +1,68 @@
+package com.example.uladzislau_nikitsin.pishapp;
+
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client extends AsyncTask<Void, Void, Void>{
+
+    private final String ip;
+    private final int port;
+    private final TextView responseTextView;
+    private String response;
+
+    public Client(final String ip, final int port, final TextView responseTextView) {
+        this.ip = ip;
+        this.port = port;
+        this.responseTextView = responseTextView;
+    }
+
+    @Override
+    protected Void doInBackground(Void... params) {
+        Socket socket = null;
+
+        try {
+            socket = new Socket(ip, port);
+
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream(1024);
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+            InputStream inputStream = socket.getInputStream();
+
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+                response+=outputStream.toString("UTF-8");
+            }
+
+        } catch (UnknownHostException ex) {
+            ex.printStackTrace();
+            response = "Unknown host exeprion " +ex.toString();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            response = "IOException: " + ex.toString();
+        } finally {
+            if (socket!=null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void results) {
+        responseTextView.setText(response);
+        super.onPostExecute(results);
+    }
+
+}
---
+++ app/src/main/java/com/example/uladzislau_nikitsin/pishapp/Server.java
@@ -0,0 +1,141 @@
+package com.example.uladzislau_nikitsin.pishapp;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+public class Server {
+    private final int PORT = 8080;
+    private MainActivity activity;
+    private ServerSocket serverSocket;
+    private String message = "";
+
+    Server(MainActivity activity) {
+        this.activity = activity;
+    }
+
+    public void start() {
+        final Thread socketServerThread = new Thread(new SocketServerThread());
+        socketServerThread.start();
+    }
+
+    public void onDestroy() {
+        if (serverSocket != null) {
+            try {
+                serverSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class SocketServerThread extends Thread {
+        private int clientCount = 0;
+        @Override
+        public void run() {
+            try {
+                serverSocket = new ServerSocket(PORT);
+
+                while(true) {
+                    Socket socket = serverSocket.accept();
+                    clientCount++;
+                    message += "#" +clientCount +" from " +socket.getInetAddress() + ":" +socket.getPort() + "\n";
+
+                    activity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            activity.serverMessages.setText(message);
+                        }
+                    });
+
+                    SocketServerReplyThread socketServerReplyThread = new SocketServerReplyThread(socket, clientCount);
+                    socketServerReplyThread.run();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    private class SocketServerReplyThread extends Thread{
+        private final int clientCount;
+        private final Socket socket;
+
+        SocketServerReplyThread(final Socket socket, final int clientCount) {
+            this.clientCount = clientCount;
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            final OutputStream outputStream;
+            final String replyMessage = "Hello from server, you are" +clientCount;
+
+            try {
+                outputStream = socket.getOutputStream();
+                final PrintStream printStream= new PrintStream(outputStream);
+                printStream.print(replyMessage);
+                printStream.close();
+
+                message += "replayed: " + replyMessage + "\n";
+
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        activity.serverMessages.setText(message);
+                    }
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
+                message += "Something wrong! " + e.toString() + "\n";
+            }
+
+            activity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    activity.serverMessages.setText(message);
+                }
+            });
+        }
+    }
+
+    public int getPort() {
+        return PORT;
+    }
+
+    public String getIp() {
+        String ip = "";
+
+        try {
+            Enumeration<NetworkInterface> enumNetworkInterfaces = NetworkInterface
+                    .getNetworkInterfaces();
+            while (enumNetworkInterfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = enumNetworkInterfaces
+                        .nextElement();
+                Enumeration<InetAddress> enumInetAddress = networkInterface
+                        .getInetAddresses();
+                while (enumInetAddress.hasMoreElements()) {
+                    InetAddress inetAddress = enumInetAddress
+                            .nextElement();
+
+                    if (inetAddress.isSiteLocalAddress()) {
+                        ip += "Server running at : "
+                                + inetAddress.getHostAddress();
+                    }
+                }
+            }
+
+        } catch (SocketException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            ip += "Something Wrong! " + e.toString() + "\n";
+        }
+        return ip;
+    }
+}
